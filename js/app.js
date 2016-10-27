9a56e2ecccd451111e9607429bcd61a0e675d249
Alex Manno <alman92@gmail.com>
2016-10-27 08:36:36 -0400

    Finish

--- js/app.js
+++ js/app.js
@@ -1,5 +1,42 @@
-"use strict";
+let postsData = [
+  { photo_url:"http://3.bp.blogspot.com/_FR4ANoG47P0/TSIE1THFskI/AAAAAAAAAYs/WBzPS26eLM0/s1600/z3.jpg", author: "LiLo", body: "Don't go back to this"}
+]
 
-(function(){
-  angular.module("wdinstagram", [])
-})();
+angular
+.module("WDInstagram", [
+  "ui.router"
+])
+.config(["$stateProvider", RouterFunction ])
+
+.controller("WDInstagramIndexController", [
+  WDInstagramIndexControllerFunction
+])
+.controller("WDInstagramShowController", [
+  WDinstagramShowControllerFunction
+])
+
+function RouterFunction($stateProvider){
+  $stateProvider
+    .state("WDInstagramIndex", {
+      url: "/WDInstagram",
+      templateUrl: "/js/ng-views/index.html",
+      controller: "WDInstagramIndexController",
+      controllerAs: "vm"
+    })
+    .state("WDInstagramShow", {
+      url: "/WDInstagram/:id",
+      templateUrl: "js/ng-views/show.html",
+      controller: "WDInstagramShowController",
+      controllerAs: "vm"
+    })
+}
+
+function WDInstagramIndexControllerFunction(){
+  this.posts = postsData
+}
+
+function WDinstagramShowControllerFunction(){
+  this.update = function(){
+
+  }
+}
